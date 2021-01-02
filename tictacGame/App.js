210d42faff6a26d922d417fcfaef9bafce67117e
KaranKataria77 <70756846+KaranKataria77@users.noreply.github.com>
2021-01-02 22:07:54 +0530

    Add files via upload
    game

---
+++ tictacGame/App.js
@@ -0,0 +1,205 @@
+/* eslint-disable semi */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable quotes */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import {Text,
+        Container,
+        Content,
+        Header,
+        Body,
+        Card,
+        H1, H3,
+        Title,
+        Button} from 'native-base';
+import Icons from './components/Icons';
+import Snackbar from 'react-native-snackbar';
+
+
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState('');
+
+  const changeItem = (itemNumber) => {
+    if (winMessage){
+      return Snackbar.show({
+        text: winMessage,
+        backgroundColor: "#000",
+        textColor: "#fff",
+      })
+    }
+
+    if (itemArray[itemNumber] === "empty"){
+      itemArray[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross)
+    } else {
+      
+    }
+    if (itemArray[0] !== "empty" &&
+        itemArray[1] !== "empty" &&
+        itemArray[2] !== "empty" &&
+        itemArray[3] !== "empty" &&
+        itemArray[4] !== "empty" &&
+        itemArray[5] !== "empty" &&
+        itemArray[6] !== "empty" &&
+        itemArray[7] !== "empty" &&
+        itemArray[8] !== "empty" &&
+        !winMessage){
+          return (
+            Snackbar.show({
+            text: "Match Drawn",
+            backgroundColor: "red",
+            textColor: "#fff",
+          }),
+          reloadGame()
+          )
+        }
+    checkIsWinner()
+  };
+
+  
+
+  const reloadGame = () => {
+    setIsCross(false)
+    setWinMessage('')
+    itemArray.fill("empty", 0, 9)
+  };
+
+  const checkIsWinner = () => {
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[1] === itemArray[2] &&
+      itemArray[0] !== 'empty'
+    ) {
+      setWinMessage(`${itemArray[0]} won`)
+    } else if (
+      itemArray[3] !== 'empty' &&
+      itemArray[3] === itemArray[4] &&
+      itemArray[4] === itemArray[5] 
+    ) {
+      setWinMessage(`${itemArray[3]} won`)
+    } else if (
+      itemArray[6] !== 'empty' &&
+      itemArray[6] === itemArray[7] &&
+      itemArray[7] === itemArray[8] 
+    ) {
+      setWinMessage(`${itemArray[6]} won`)
+    } else if (
+      itemArray[0] !== 'empty' &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6] 
+    ) {
+      setWinMessage(`${itemArray[0]} won`)
+    } else if (
+      itemArray[1] !== 'empty' &&
+      itemArray[1] === itemArray[4] &&
+      itemArray[4] === itemArray[7] 
+    ) {
+      setWinMessage(`${itemArray[1]} won`)
+    } else if (
+      itemArray[2] !== 'empty' &&
+      itemArray[2] === itemArray[5] &&
+      itemArray[5] === itemArray[8] 
+    ) {
+      setWinMessage(`${itemArray[2]} won`)
+    } else if (
+      itemArray[0] !== 'empty' &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8] 
+    ) {
+      setWinMessage(`${itemArray[0]} won`)
+    } else if (
+      itemArray[2] !== 'empty' &&
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6] 
+    ) {
+      setWinMessage(`${itemArray[2]} won`)
+    }
+  };
+
+  return (
+    <Container style={{backgroundColor: "333945", padding: 5}}>
+      <Header>
+        <Body>
+          <Title>
+            LCO Tic Tac Toe
+          </Title>
+        </Body>
+      </Header>
+      <Content>
+        <View style={styles.grid}>
+          {itemArray.map((item, index) => (
+            <TouchableOpacity 
+              style={styles.box}
+              key={index}
+              onPress={() => changeItem(index)}
+            >
+              <Card style={styles.card}>
+                <Icons name={item} />
+              </Card>
+            </TouchableOpacity>
+          ))}
+        </View>
+        {winMessage ? 
+        (
+          <View>
+          <H1 style={styles.message}>{winMessage}</H1>
+          <Button
+           onPress={reloadGame}
+           primary
+           block
+           rounded>
+            <Text>Reload Game</Text>
+          </Button>
+          </View>
+        ) : 
+        (
+          <H3 style={styles.message}>
+            {isCross ? 'Cross' : "Circle"} turns
+          </H3>
+        )}
+      </Content>
+    </Container>
+  );
+};
+
+const styles = StyleSheet.create({
+  grid:{
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 20,
+  },box: {
+    width: "33%",
+    marginBottom: 6,
+  },
+  card: {
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  message: {
+    textAlign: "center",
+    textTransform: "uppercase",
+    color: "#fff",
+    marginTop: 20,
+    backgroundColor: "#4652b3",
+    paddingVertical: 10,
+    marginVertical: 10,
+  },
+});
+
+export default App;
